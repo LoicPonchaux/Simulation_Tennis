a2f6c39581ead86197b62ce1e6c18a014cce50ca
LoicPonchaux <loicponchaux@yahoo.fr>
2020-02-13 12:10:12 +0100

    Ajout de la classe joueur

---
+++ Player/player.py
@@ -0,0 +1,13 @@
+import Player.identity as identity
+
+import Player.features as features
+
+
+class Player(identity.Identity, features.Features):
+
+    def __init__(self, lastname, firstname, age, forehand, backhand, service, back):
+        identity.Identity.__init__(self, lastname, firstname, age)
+        features.Features.__init__(self, forehand, backhand, service, back)
+
+    def player_presentation(self):
+        return self.firstname + " " + self.lastname
