4389e2be9eb16bf1d3946c7b91a2c2547b9fac9e
LoicPonchaux <loicponchaux@yahoo.fr>
2020-02-13 10:57:42 +0100

    Ajout d'une classe pour les stats techniques

---
+++ Player/features.py
@@ -0,0 +1,8 @@
+class Features :
+    def __init__(self, forehand, backhand, service, back):
+        self.forehand = forehand
+        self.backhand = backhand
+        self.service = service
+        self.back = back
+
+
