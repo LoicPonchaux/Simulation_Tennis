5b388e7795d7199cc27a3aac6c031f107e7706cc
loic <loicponchaux@yahoo.fr>
2020-04-10 14:01:45 +0200

    Ajout commentaires

--- Score/game.py
+++ Score/game.py
@@ -1,15 +1,26 @@
 class Game():
+    """
+    Class pour gerer le nombre de jeux gagner
+    :param number: compteur
+    :type number: int
+    """
 
     def __init__(self):
         self.number = 0
 
-    """Méthode pour incrémenter un jeu"""
-
     def countGame(self):
+        """
+        Incremente de 1 le nombre de jeu gagner
+        :return:
+        """
         self.number += 1
 
-    """Méthode pour l'affichage des jeux  du set"""
-
     def compareGame(self, other):
+        """
+        Determine qui a le plus de jeu gagner
+        En comparant le nombre de jeu par personne
+        :param other: Joueur a comparer
+        :return: Celui qui a le plus de jeux
+        """
         if isinstance(other, self.__class__):
             return str(other.number) + " " + str(self.number)
