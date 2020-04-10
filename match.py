ed07ddb5166aaaf04452d838a2fa4e2462a480ea
LoicPonchaux <loicponchaux@yahoo.fr>
2020-04-10 15:08:14 +0200

    Ajout fonction

--- match.py
+++ match.py
@@ -4,7 +4,24 @@ import random
 
 
 class Match:
+    """
+    On joue le match ici on a donc :
+    -   Joueur 1
+    -   Joueur 2
+    -   Le score
+    -   Nombre de Jeux pour gagner un set
+    -   Nombre de Sets pour gagner le match
+    -   Nombre de Jeux jouer
+    -   Vainqueur du match
+    """
     def __init__(self, lastNameJ1, nameJ1, lastNameJ2, nameJ2):
+        """
+        :param lastNameJ1: str
+        :param nameJ1: str
+        :param lastNameJ2: str
+        :param nameJ2: str
+        """
+
         self.J1 = player.Player(lastNameJ1, nameJ1, 37, 85, 85, 50, 50)
         self.J2 = player.Player(lastNameJ2, nameJ2, 34, 98, 75, 50, 50)
         self.score = score.Score(self.J1, self.J2)
@@ -14,58 +31,91 @@ class Match:
         self.winner = None
 
     def displayMatch(self):
+        """
+        Présentation du match
+        :return: L'affiche du jour
+        """
         return self.J1.player_presentation() + " VS " + self.J2.player_presentation()
 
 
 
     def play(self):
-        match = True
-        set = 1;
-        while (match):
+        """
+        On joue le match
+        :return: str
+            Le score du match
+        """
+
+        # Tant que le match n'a pas de vainqueur on joue
+        match = False
+        # Début du premier set
+        set = 1
+        while (match == False):
             if (set == 1):
                 match = self.playGame(self.score.set1)
-                if (match == False):
+                if (match == True):
                     set = 2
                     self.nbJeu = 6
-                    match = True
+                    match = False
             if (set == 2):
                 match = self.playGame(self.score.set2)
                 if (self.score.set1.winner == self.score.set2.winner):
                     self.winner = self.score.set1.winner
                     return self.score.displayScore()
-                if (match == False):
+                if (match == True):
                     set = 3
                     self.nbJeu = 6
-                    match = True
+                    match = False
             if (set == 3):
                 match = self.playGame(self.score.set3)
-        # return self.resultMatch() + " " + self.score.displayScore()
         self.winner = self.score.set3.winner
         return self.score.displayScore()
 
     def playGame(self, set):
+        """
+        Permet de jouer un jeu
+        :param set: l'objet set
+        :return: Fin du set ou non
+        """
+        J1, J2 = self.determineServe()
+        if (J1 > J2):
+            set.gameJ1.countGame()
+        else:
+            set.gameJ2.countGame()
+        self.numberGame += 1
+        return self.determineEndSet(set)
+
+    def determineServe(self):
+        """
+        On détermine qui a le service
+        :return: int, int
+            Score du calcul de chaque joueur
+        """
+
         if self.numberGame % 2 == 0:
             J1 = random.randint(0, 10) * self.J1.service
             J2 = random.randint(0, 10) * self.J2.back
         else:
             J1 = random.randint(0, 10) * self.J1.back
             J2 = random.randint(0, 10) * self.J2.service
-        if (J1 > J2):
-            set.gameJ1.countGame()
-            # self.score.gameJ1.countGame()
-        else:
-            set.gameJ2.countGame()
-            # self.score.gameJ2.countGame()
-        self.numberGame += 1
+        return J1, J2
+
+    def determineEndSet(self, set):
+        """
+        Détermine si le set est fini ou non
+        :return: bool
+            Fin du set ou non
+        """
         if (set.gameJ1.number == 5 and set.gameJ2.number == 5):
             self.nbJeu += 1
+            return False
         if (set.gameJ1.number == self.nbJeu or set.gameJ2.number == self.nbJeu):
             if (set.gameJ1.number == self.nbJeu):
                 set.defineWinner(self.J1)
             else:
                 set.defineWinner(self.J2)
-            return False
-        return True
+            return True
+        return False
 
 J1lastname = input("Nom Joueur 1\n")
 J1name = input("Prénom Joueur 1\n")
